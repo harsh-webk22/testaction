6f68399f3d74bc58b15240a35042c00761765efc
harsh-webk22 <aggarwalharsh912@gmail.com>
2024-01-31 22:18:18 +0530

    first2

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ app.get("/", (req, res) => {
   res.send("Hi");
 });
 
+app.get("/1", (req, res) => {
+    res.send("Hihjcvn");
+  });
+  
 app.listen(9000, () => {
   console.log("Server running!");
 });
